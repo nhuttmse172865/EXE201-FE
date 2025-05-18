bb70717a37cc3d718fd00b42b3fc36f791ef427b
nhuttmse172865 <nhuttmse172865@fpt.edu.vn>
2025-05-19 02:26:06 +0700

    Add header, 404 page

--- src/pages/customer/home/Home.jsx
+++ src/pages/customer/home/Home.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import Footer from "../../../components/customer/footer/Footer";
+import Header from "../../../components/customer/header/Header";
+import Body from "../../../components/customer/home/Body";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
