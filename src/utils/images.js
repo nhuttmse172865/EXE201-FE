8a23214b3074c940a251aca92cd2c17ccb5442fa
nhuttmse172865 <nhuttmse172865@fpt.edu.vn>
2025-05-23 00:18:30 +0700

    add veterinary on home page

--- src/utils/images.js
+++ src/utils/images.js
@@ -1,19 +1,23 @@
-import logo from '../assets/images/exe-logo.png'
-import facebook from '../assets/images/facebook.png'
-import linkedin from '../assets/images/linkedin.png'
-import tiktok from '../assets/images/tiktok.png'
-import xTwitter from '../assets/images/x-twitter.png'
-import instagram from '../assets/images/instagram.png'
-import bannerHeader from '../assets/images/banner-header.png'
+import logo from "../assets/images/exe-logo.png";
+import facebook from "../assets/images/facebook.png";
+import linkedin from "../assets/images/linkedin.png";
+import tiktok from "../assets/images/tiktok.png";
+import xTwitter from "../assets/images/x-twitter.png";
+import instagram from "../assets/images/instagram.png";
+import location from "../assets/images/location.png";
+import star from "../assets/images/star.png";
+import arrowRight from "../assets/images/arrow-right.png";
 
 const IMAGES = {
-    logo,
-    facebook,
-    linkedin,
-    tiktok,
-    xTwitter,
-    instagram,
-    bannerHeader
-}
+  logo,
+  facebook,
+  linkedin,
+  tiktok,
+  xTwitter,
+  instagram,
+  location,
+  star,
+  arrowRight,
+};
 
 export default IMAGES;
