9fb1ccdc628846c67c71d725ad6f7e4b867ca3e1
nhuttmse172865 <nhuttmse172865@fpt.edu.vn>
2025-05-30 01:14:46 +0700

    add service featured

--- src/components/customer/home/Body.jsx
+++ src/components/customer/home/Body.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import HeadSection from "./head-section/HeadSection";
 import ServiceSection from "./service-section/ServiceSection";
 import VeterinarySection from "./veterinary-section/VeterinarySection";
-
+import FeaturedService from "./featured-services-section/FeaturedService";
 const Body = () => {
   return (
     <div className=" min-h-[calc(100vh-200px)]">
       <HeadSection />
       <ServiceSection />
+      <FeaturedService />
       <VeterinarySection />
     </div>
   );
--- src/components/customer/home/head-section/content/clinics/Clinics.jsx
+++ src/components/customer/home/head-section/content/clinics/Clinics.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Clinics = () => {
   return (
-    <div className='h-[100%] w-[220px] bg-[rgba(0,0,0,0.05)] rounded-[.375rem]'>
-
+    <div className="relative h-[100%] w-[220px] bg-[rgba(0,0,0,0.05)] rounded-[16px]">
+      <div className="bg-white w-fit px-2.5 rounded-[.375rem] flex justify-center items-center py-0.5 absolute right-[10px] top-[10px]">
+        <span className="text-[11px] ">4.9</span>
+      </div>
+      <div className="bg-amber-300 w-[]"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Clinics
+export default Clinics;
