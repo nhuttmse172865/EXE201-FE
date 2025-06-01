27436404a998888bb2668e02f97f50c1d3fb3ac2
nhuttmse172865 <nhuttmse172865@fpt.edu.vn>
2025-06-01 23:34:24 +0700

    redesign

--- src/components/login/content/LoginContent.jsx
+++ src/components/login/content/LoginContent.jsx
@@ -1,45 +1,42 @@
 import React, { useEffect, useState } from "react";
 
 const LoginContent = () => {
+  const [widthContent, setWidthContent] = useState();
+  const [widthContainer, setWidthContainer] = useState();
+
+  const handleWidthContentLogin = () => {
+    const widthContainerlogin =
+      document.getElementById("container-login")?.clientWidth;
+    const widthContainerFormLogin = document.getElementById(
+      "container-form-login"
+    ).clientWidth;
+    if (widthContainerlogin) {
+      setWidthContainer(
+        (window.innerWidth - widthContainerlogin) / 2 +
+          (widthContainerlogin - widthContainerFormLogin)
+      );
+      setWidthContent(widthContainerlogin - widthContainerFormLogin - 20);
+    }
+  };
+
+  window.addEventListener("resize", () => {
+    handleWidthContentLogin();
+  });
+
+  useEffect(() => {
+    if (!widthContent) {
+      handleWidthContentLogin();
+    }
+  });
 
   return (
     <div
-      className="relative bg-[rgba(0,0,0,0.1)] w-full rounded-tr-[25px] rounded-tl-[25px] rounded-bl-[25px] overflow-hidden"
+      className="relative bg-[rgba(0,0,0,0.1)] rounded-tl-[25px] overflow-hidden h-full"
       style={{
-        height: "calc(100% - 10px)",
+        width: `${widthContainer - 28}px`,
       }}
     >
-      <div className="absolute w-[55%] h-[100px] bg-white bottom-0 right-0 rounded-tl-[25px]"
-      
-      >
-        <div className="absolute right-full bottom-0 w-[25px] h-[25px]">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="100%"
-            height="100%"
-            viewBox="0 0 50 50"
-            fill="none"
-          >
-            <path d="M50 0.5C50 17 40 50 0 50H50V0.5Z" fill="white" />
-          </svg>
-        </div>
-        <div className="absolute right-0 bottom-full w-[25px] h-[25px]">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="100%"
-            height="100%"
-            viewBox="0 0 50 50"
-            fill="none"
-          >
-            <path d="M50 0.5C50 17 40 50 0 50H50V0.5Z" fill="white" />
-          </svg>
-        </div>
-
-        <h1></h1>
-
-      </div>
-
-    
+     
     </div>
   );
 };
