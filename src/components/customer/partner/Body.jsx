c156c8174e7e9715661c6904e12b5bd6b5e0ea4c
nhuttmse172865 <nhuttmse172865@fpt.edu.vn>
2025-06-03 22:44:36 +0700

    add offer section page

--- src/components/customer/partner/Body.jsx
+++ src/components/customer/partner/Body.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeadSection from './head-section/HeadSection'
+import OfferSection from './offers-section/OfferSection'
 
 const Body = () => {
   return (
     <div>
         <HeadSection />
+        <OfferSection />
     </div>
   )
 }
