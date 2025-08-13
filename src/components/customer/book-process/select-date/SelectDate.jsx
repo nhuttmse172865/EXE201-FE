4a505d7180c245c1874f47ac15df0098cab51d2b
Tran Minh Nhut <nhuttmse172865@fpt.edu.vn>
2025-08-13 11:06:48 +0700

    50% booking process

---
+++ src/components/customer/book-process/select-date/SelectDate.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const SelectDate = () => {
+  return (
+     <div className="">
+        <div className="flex gap-2.5 items-center">
+          <div className="w-[2px] bg-(--color-primary-100) h-[20px]"></div>
+          <h1 className="font-family-playfair-display text-[18px] font-medium text-[rgba(0,0,0,0.8)]">
+            Select Date
+          </h1>
+        </div>
+       
+      </div>
+  )
+}
+
+export default SelectDate
